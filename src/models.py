c311b9b36ff955d427dc02e5b7f5c5947782a8d6
Slevin <slevin.jstark@gmail.com>
2025-12-10 07:15:29 +0100

    Add User model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from typing import List
 from typing import Optional
-from sqlalchemy import String, ForeignKey, Table, Column, Integer
+from sqlalchemy import String, ForeignKey, Table, Column, Integer, Boolean
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -34,4 +34,15 @@ class Project(Base):
 
     techs: Mapped[List["Tech"]] = relationship(secondary=project_techs, back_populates='projects')
 
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(100))
+    is_admin: Mapped[bool] = mapped_column(Boolean)
+
+    # Those two actually for possible future functional extension
+    fullname: Mapped[str | None] = mapped_column(String(150), nullable=True)
+    email: Mapped[str | None] = mapped_column(String(100), unique=True, nullable=True)
 
