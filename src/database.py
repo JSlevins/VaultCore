e7fe64d7e7f152384a519b5df9a45a9e160b8e59
Slevin <slevin.jstark@gmail.com>
2025-12-10 07:15:08 +0100

    Add database

---
+++ src/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///database.db'
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    echo=True
+)
+
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
