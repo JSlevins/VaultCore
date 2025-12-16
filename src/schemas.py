98aff7b106fa33deaeedbe031df3c297565cf0e8
Slevin <slevin.jstark@gmail.com>
2025-12-16 14:33:10 +0100

    add field_validator

--- src/schemas.py
+++ src/schemas.py
@@ -1,14 +1,26 @@
 from typing import Annotated
 
-from pydantic import BaseModel, StringConstraints
+from pydantic import BaseModel, StringConstraints, field_validator
 from pydantic.config import ConfigDict
 
+class BaseSchema(BaseModel):
+
+    @field_validator("description")
+    @classmethod
+    def empty_string_to_none(cls, v):
+        if v is None:
+            return None
+        if isinstance(v, str):
+            v = v.strip()
+            return v if v else None
+        return v
+
 # Tech model
-class TechAdd(BaseModel):
+class TechCreate(BaseSchema):
     name: Annotated[str, StringConstraints(max_length=50)]
     description: Annotated[str | None, StringConstraints(max_length=1000)] = None
 
-class TechUpdate(BaseModel):
+class TechUpdate(BaseSchema):
     name: Annotated[str | None, StringConstraints(max_length=50)] = None
     description: Annotated[str | None, StringConstraints(max_length=1000)] = None
 
@@ -21,11 +33,11 @@ class TechRead(BaseModel):
 
 
 # Project model
-class ProjectAdd(BaseModel):
+class ProjectCreate(BaseSchema):
     name: Annotated[str, StringConstraints(max_length=100)]
     description: Annotated[str | None, StringConstraints(max_length=1000)] = None
 
-class ProjectUpdate(BaseModel):
+class ProjectUpdate(BaseSchema):
     name: Annotated[str | None, StringConstraints(max_length=100)] = None
     description: Annotated[str | None, StringConstraints(max_length=1000)] = None
 
