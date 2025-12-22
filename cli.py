9d87b764f7a47e6e887b2250e982c381b0604f7f
Slevin <slevin.jstark@gmail.com>
2025-12-22 18:53:34 +0100

    Add Admin user creation, Password hashing, command cli interface.

---
+++ cli.py
@@ -0,0 +1,25 @@
+import sys
+from src.commands import create_admin
+from src.database import Session
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python cli.py <command> [<args>]")
+        print("Available commands: 'create-admin'")
+        return
+
+    command = sys.argv[1]
+
+    if command == "create-admin":
+        db = Session()
+        try:
+            create_admin(db)
+        finally:
+            db.close()
+
+    else:
+        print(f"{command} is not a valid command.")
+
+if __name__ == "__main__":
+    main()
---
+++ src/commands.py
@@ -0,0 +1,53 @@
+from getpass import getpass
+import re
+
+from src.models import User
+from src.security import hash_password
+
+def create_admin(db_session):
+    admin_exists = db_session.query(User).filter_by(is_admin=True).first()
+    if admin_exists:
+        print("Admin already exists. Aborting...")
+        return
+
+    # Inputs
+    username = input("Username: ").strip()
+    password = getpass("Password: ")
+    password2 = getpass("Confirm Password: ")
+    email = input("Email address: ").strip()
+
+    # Inputs validation
+    if user_creation_validation(username, password, password2, email):
+        admin = User(
+            username=username,
+            password_hash=hash_password(password),
+            email=email,
+            is_admin=True
+        )
+
+        db_session.add(admin)
+        db_session.commit()
+        print("Admin user was successfully created")
+
+def user_creation_validation(username: str, password: str, password2:str, email: str) -> bool:
+    error_msg = []
+    if not username:
+        error_msg.append("Username required.")
+
+    if password != password2:
+        error_msg.append("Passwords don't match.")
+
+    if len(password) < 8:
+        error_msg.append("Password must be at least 8 characters long.")
+
+    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    if not re.match(pattern, email):
+        error_msg.append("Invalid email.")
+
+    if error_msg:
+        print(f"\nAdmin wasn't created:")
+        for i, msg in enumerate(error_msg, 1):
+            print(f"{i}. {msg}")
+        print("\n")
+        return False
+    return True
