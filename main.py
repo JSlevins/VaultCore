c5da515cf9036c784a1435f8401c71de62d7ed6b
Slevin <slevin.jstark@gmail.com>
2026-01-13 17:34:17 +0100

    Update to current router's count

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from src.routers import techs, projects
+from src.routers import auth, projects, techs
 
 app = FastAPI(title="VaultCore API")
 
-app.include_router(techs.router, tags=["Techs"])
-app.include_router(projects.router, tags=["Projects"])
+app.include_router(auth.auth_router, tags=["Auth"])
+app.include_router(techs.techs_router, tags=["Techs"])
+app.include_router(projects.project_router, tags=["Projects"])
+
